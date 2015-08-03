7e17a4c0ee7b07db77843c7184cbe39f6499977b
brunokbcao <brunokbcao@gmail.com>
2015-08-03 13:24:51 -0300

    Mais algumas brincadeiras com Mapa

--- app/src/main/java/br/com/ourfi/ourfi/OurFiActivity.java
+++ app/src/main/java/br/com/ourfi/ourfi/OurFiActivity.java
@@ -1,193 +1,33 @@
 package br.com.ourfi.ourfi;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.CircleOptions;
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.List;
 
 
-public class OurFiActivity extends AppCompatActivity {
-
-    private static final String TAG = "ourfi";
-    private WifiManager wifiManager;
-    private String connectedSsidName;
-    private List<ScanResult> scanResults;
-    private int res;
-
-    public void connectToAP(String networkSSID, String networkPass) {
-        Log.i(TAG, "* connectToAP");
-
-        WifiConfiguration wifiConfiguration = new WifiConfiguration();
-
-        Log.d(TAG, "# password " + networkPass);
-
-        for (ScanResult result : scanResults) {
-            if (result.SSID.equals(networkSSID)) {
-
-                String securityMode = getScanResultSecurity(result);
-
-                if (securityMode.equalsIgnoreCase("OPEN")) {
-
-                    wifiConfiguration.SSID = "\"" + networkSSID + "\"";
-                    wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-                    int res = wifiManager.addNetwork(wifiConfiguration);
-                    Log.d(TAG, "# add Network returned " + res);
-
-                    boolean b = wifiManager.enableNetwork(res, true);
-                    Log.d(TAG, "# enableNetwork returned " + b);
-
-                    wifiManager.setWifiEnabled(true);
-
-                } else if (securityMode.equalsIgnoreCase("WEP")) {
-
-                    wifiConfiguration.SSID = "\"" + networkSSID + "\"";
-                    wifiConfiguration.wepKeys[0] = "\"" + networkPass + "\"";
-                    wifiConfiguration.wepTxKeyIndex = 0;
-                    wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-                    wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-                    int res = wifiManager.addNetwork(wifiConfiguration);
-                    Log.d(TAG, "### 1 ### add Network returned " + res);
-
-                    boolean b = wifiManager.enableNetwork(res, true);
-                    Log.d(TAG, "# enableNetwork returned " + b);
-
-                    wifiManager.setWifiEnabled(true);
-                }
-
-                wifiConfiguration.SSID = "\"" + networkSSID + "\"";
-                wifiConfiguration.preSharedKey = "\"" + networkPass + "\"";
-                wifiConfiguration.hiddenSSID = true;
-                wifiConfiguration.status = WifiConfiguration.Status.ENABLED;
-                wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-                wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-                wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-                wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-                wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-                wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-                wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-
-                res = wifiManager.addNetwork(wifiConfiguration);
-                Log.d(TAG, "### 2 ### add Network returned " + res);
-
-                wifiManager.enableNetwork(res, true);
-
-                /*boolean changeHappen = wifiManager.saveConfiguration();
-
-                if(res != -1 && changeHappen){
-                    Log.d(TAG, "### Change happen");
-
-                    connectedSsidName = networkSSID;
-
-                }else{
-                    Log.d(TAG, "*** Change NOT happen");
-                }*/
-
-                wifiManager.setWifiEnabled(true);
-            }
-        }
-    }
-
-    public String getScanResultSecurity(ScanResult scanResult) {
-        Log.i(TAG, "* getScanResultSecurity");
-
-        final String cap = scanResult.capabilities;
-        final String[] securityModes = { "WEP", "PSK", "EAP" };
-
-        for (int i = securityModes.length - 1; i >= 0; i--) {
-            if (cap.contains(securityModes[i])) {
-                return securityModes[i];
-            }
-        }
-
-        return "OPEN";
-    }
+public class OurFiActivity extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_our_fi);
 
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        wifiManager.startScan();
-        scanResults = wifiManager.getScanResults();
-        for (ScanResult sc : scanResults) {
-            if (sc.SSID.equals("NT_INTERNA")) {
-                connectToAP("NT_INTERNA", "neurotechciti");
-            }
-        }
-
-        //Intent it = new Intent(this, MapsActivity.class);
-        //startActivity(it);
-        mapa();
-    }
-
-    private void mapa() {
-        MapFragment x = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment);
-        GoogleMap mMap = x.getMap();
-
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Location myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true));
-        LatLng latLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-        mMap.setMyLocationEnabled(true);
-        mMap.setContentDescription("TESTE");
-        mMap.getUiSettings().setZoomControlsEnabled(false);
-        mMap.getUiSettings().setZoomGesturesEnabled(false);
-
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
-        mMap.addCircle(new CircleOptions().center(latLng).radius(30).fillColor(0x2FFF0000).strokeWidth(1f).strokeColor(0xFFFF0000));
+        Utils.initServices(this);
+        Utils.setupMap(this);
+        Utils.enableLocation(this);
+        Utils.setupNotification(this);
     }
 
     @Override
     protected void onDestroy() {
+        Utils.finish(this);
         super.onDestroy();
-        wifiManager.removeNetwork(res);
-        System.out.println("onDestroy");
-        NotificationManager notMan = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notMan.cancel(1);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        System.out.println("onPause");
-
-        NotificationManager notMan = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Intent notificationIntent = new Intent(this, getClass());
-        notificationIntent.setAction(Intent.ACTION_MAIN);
-        notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        PendingIntent pi = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        Notification not = new Notification.Builder(this)
-                .setContentTitle("OurFi")
-                .setContentText("Estamos Conectados :D")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentIntent(pi)
-                .build();
-        notMan.notify(1, not);
     }
 
     @Override
@@ -210,5 +50,27 @@ public class OurFiActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "Location Changed: " + location.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        System.out.println("Location onStatusChanged: " + provider
+                + "\n" + status
+                + "\n" + extras.toString());
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        System.out.println("Location onProviderEnabled: " + provider);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        System.out.println("Location onProviderDisabled: " + provider);
     }
 }
---
+++ app/src/main/java/br/com/ourfi/ourfi/Utils.java
@@ -0,0 +1,235 @@
+package br.com.ourfi.ourfi;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Looper;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Classe que conterá os métodos utilitários que precisarmos
+ */
+public class Utils {
+
+    private static WifiManager wifiManager;
+    private static LocationManager locationManager;
+    private static NotificationManager notificationManager;
+
+    private static int notificationId;
+    private static int networkId;
+
+    public static void initServices(Context context) {
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public static List<ScanResult> listWifiNetworks() {
+        if (wifiManager != null) {
+            wifiManager.startScan();
+            return wifiManager.getScanResults();
+        }
+        return new ArrayList<>();
+    }
+
+    public static int connectToAP(String networkSSID, String networkPass) {
+        int res = -1;
+
+        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+        for (ScanResult result : listWifiNetworks()) {
+            if (result.SSID.equals(networkSSID)) {
+
+                String securityMode = getScanResultSecurity(result);
+
+                if (securityMode.equalsIgnoreCase("OPEN")) {
+                    wifiConfiguration.SSID = "\"" + networkSSID + "\"";
+                    wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                    res = wifiManager.addNetwork(wifiConfiguration);
+                    boolean b = wifiManager.enableNetwork(res, true);
+                    wifiManager.setWifiEnabled(true);
+                } else if (securityMode.equalsIgnoreCase("WEP")) {
+                    wifiConfiguration.SSID = "\"" + networkSSID + "\"";
+                    wifiConfiguration.wepKeys[0] = "\"" + networkPass + "\"";
+                    wifiConfiguration.wepTxKeyIndex = 0;
+                    wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                    wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                    res = wifiManager.addNetwork(wifiConfiguration);
+                    boolean b = wifiManager.enableNetwork(res, true);
+                    wifiManager.setWifiEnabled(true);
+                }
+
+                wifiConfiguration.SSID = "\"" + networkSSID + "\"";
+                wifiConfiguration.preSharedKey = "\"" + networkPass + "\"";
+                wifiConfiguration.hiddenSSID = true;
+                wifiConfiguration.status = WifiConfiguration.Status.ENABLED;
+                wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+                wifiConfiguration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                wifiConfiguration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                wifiConfiguration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                wifiConfiguration.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+
+                res = wifiManager.addNetwork(wifiConfiguration);
+                wifiManager.enableNetwork(res, true);
+                wifiManager.setWifiEnabled(true);
+            }
+        }
+        return res;
+    }
+
+    private static String getScanResultSecurity(ScanResult scanResult) {
+        final String cap = scanResult.capabilities;
+        final String[] securityModes = { "WEP", "PSK", "EAP" };
+
+        for (int i = securityModes.length - 1; i >= 0; i--) {
+            if (cap.contains(securityModes[i])) {
+                return securityModes[i];
+            }
+        }
+        return "OPEN";
+    }
+
+    public static void disconnectFromAP(int res) {
+        if (wifiManager != null) {
+            wifiManager.removeNetwork(res);
+        }
+    }
+
+    public static void enableLocation(LocationListener locationListener) {
+        if (locationManager != null) {
+            locationManager.requestSingleUpdate(new Criteria(), locationListener, Looper.getMainLooper());
+        }
+    }
+
+    public static void disableLocation(LocationListener locationListener) {
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    public static void setupMap(final Activity context) {
+        if (locationManager != null) {
+            GoogleMap map = ((MapFragment) context.getFragmentManager().findFragmentById(R.id.fragment)).getMap();
+            Location myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true));
+            LatLng latLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(false);
+            map.getUiSettings().setAllGesturesEnabled(false);
+            map.getUiSettings().setMapToolbarEnabled(false);
+
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+            final Circle c =  map.addCircle(new CircleOptions().center(latLng).radius(30).fillColor(0x2FFF0000).strokeWidth(1f).strokeColor(0xFFFF0000));
+            map.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .alpha(0)
+                    .infoWindowAnchor(0.5f, 0.5f));
+
+            map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    LatLng position = marker.getPosition();
+                    marker.setTitle("Lista de redes nesta posição...");
+                    marker.setSnippet("Rede A\nRede B");
+                    marker.showInfoWindow();
+                    System.out.println(position);
+                    LatLng center = c.getCenter();
+                    double radius = c.getRadius();
+                    float[] distance = new float[1];
+                    Location.distanceBetween(position.latitude, position.longitude, center.latitude, center.longitude, distance);
+                    boolean clicked = distance[0] < radius;
+                    System.out.println("Circulo clicado? = " + clicked);
+                    return false;
+                }
+            });
+
+            map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+                @Override
+                public View getInfoWindow(Marker arg0) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    TextView info = new TextView(context);
+                    info.setTextColor(Color.DKGRAY);
+                    info.setText(marker.getSnippet());
+                    return info;
+                }
+            });
+
+            map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    System.out.println("Aproveitar para atualizar as redes ao meu redor...");
+                    return false;
+                }
+            });
+        }
+    }
+
+    public static int setupNotification(Context context) {
+        if (notificationManager != null) {
+            Intent notificationIntent = new Intent(context, context.getClass());
+            notificationIntent.setAction(Intent.ACTION_MAIN);
+            notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            PendingIntent pi = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+            Notification not = new Notification.Builder(context)
+                    .setContentTitle("OurFi")
+                    .setOngoing(true)
+                    .setContentText("Free WiFi's around you")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pi)
+                    .setPriority(Notification.PRIORITY_MIN)
+                    .build();
+
+            notificationId = 1;
+            notificationManager.notify(notificationId, not);
+        }
+        return notificationId;
+    }
+
+    public static void finish(Context context) {
+        if (notificationManager != null) {
+            notificationManager.cancel(notificationId);
+        }
+
+        if (locationManager != null) {
+            locationManager.removeUpdates((LocationListener) context);
+        }
+
+        if (wifiManager != null) {
+            wifiManager.removeNetwork(networkId);
+        }
+    }
+}
